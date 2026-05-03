fdd141841420d3384aecf2c7dfca028304b19c61
Maite Aldenkamp <r0985959@student.thomasmore.be>
2026-05-03 20:41:26 +0200

    feat (pauseIcons): add pause icon mapping for various activities

---
+++ frontend/src/utils/pauseIconMap.js
@@ -0,0 +1,21 @@
+import posture from "../assets/icons_groen/houding_check_groen.svg";
+import win from "../assets/icons_groen/name_one_win_groen.svg";
+import hand from "../assets/icons_groen/hand_stretch_groen.svg";
+import touch from "../assets/icons_groen/hart_default_groen.svg";
+import water from "../assets/icons_groen/koffie_groen.svg";
+import eye from "../assets/icons_groen/oog_reset_groen.svg";
+import breath from "../assets/icons_groen/ademhaling_groen.svg";
+import walk from "../assets/icons_groen/korte_wandeling_groen.svg";
+import stretch from "../assets/icons_groen/stretchen_groen.svg";
+
+export const pauseIconMap = {
+  posture,
+  win,
+  hand,
+  touch,
+  water,
+  eye,
+  breath,
+  walk,
+  stretch,
+};
