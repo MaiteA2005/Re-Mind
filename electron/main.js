44ee59af4780093e7f54e4f0c9026db14790b286
Maite Aldenkamp <r0985959@student.thomasmore.be>
2026-05-05 01:06:10 +0200

    Refactor code structure for improved readability and maintainability

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 
+const path = require("path");
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
@@ -8,6 +10,7 @@ function createWindow() {
       nodeIntegration: false,
       contextIsolation: true,
     },
+    icon: path.join(__dirname, "icon.ico"),
   });
 
   const isDev = !app.isPackaged;
